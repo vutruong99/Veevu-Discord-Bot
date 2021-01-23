e63d8313ad6a9c292e09e5234b9f9fda9d7ad39e
Thi <truongsithivu99@gmail.com>
2021-01-23 13:32:57 +0700

    Changed country names into country codes

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const {Client, MessageAttachment} = require('discord.js')
 const bot = new Client()
-
+const request = require('request')
+const cheerio = require('cheerio')
 const fs = require('fs');
-
 const https = require('https')
-const token = ""
 
-const generalCovidURL = "https://corona.lmao.ninja/all"
-const countryCovidURL = "https://corona.lmao.ninja/countries/"
+var token = require('./auth.json') 
+
+const generalCovidURL = "https://corona.lmao.ninja/v2/all"
+const countryCovidURL = "https://corona.lmao.ninja/v2/countries"
 const PREFIX = 'vv'
 
+/*Total cases, deaths and recovered cases*/
 let generalCovid = ''
+
+//Cases, deaths, recovered and more by countries
 let countryCovid = ''
 
+//ISO3166 alpha 2 country code
 let countryCodeRaw = fs.readFileSync("ISO3166-1.alpha2.json")
 let countryCode = JSON.parse(countryCodeRaw)
 
@@ -25,9 +30,8 @@ function invert(json){
 }
 
 
-countryCode = invert(countryCode)
-console.log(countryCode)
 
+//get general Conora data and country Corona
 https.get(generalCovidURL, (resp) => {
 
   resp.on('data', (chunk) => {
@@ -56,12 +60,52 @@ https.get(countryCovidURL, (resp) => {
     console.log("Error: " + err.message);
 });
 
+function isNormalInteger(str) {
+    var n = Math.floor(Number(str));
+    return n !== Infinity && String(n) === str && n >= 0;
+}
 
+//Random image, code form CodeLyon
+function image(message){
+ 
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "food",
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+ 
+    request(options, function(error, response, responseBody) {
+        if (error) {
+            return;
+        }
+ 
+        $ = cheerio.load(responseBody);
+ 
+        var links = $(".image a.link");
+ 
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+       
+        console.log(urls);
+ 
+        if (!urls.length) {
+            return;
+        }
+        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
+    });
+ 
+ 
+}
+
+//Print when ready
 bot.on('ready', ()=> {
     console.log("Veevu is vee-vued")
 })
 
 
+//Commands
 bot.on('message', msg=> {
     
     if (msg.content.startsWith(PREFIX)) {
@@ -79,18 +123,40 @@ bot.on('message', msg=> {
                 msg.channel.bulkDelete(args[2])
                 break
             case 'img':
-                const attachment = new MessageAttachment('./test.jpg')
-                msg.channel.send(attachment)
+                // if (args[2] == "author") {
+                //     const attachment = new MessageAttachment('./eva.jpg')
+                //     msg.channel.send(attachment)
+                //     break
+                // }  
+                // const attachment = new MessageAttachment('./test.jpg')
+                // msg.channel.send(attachment)
+                image(msg)
+                break
+            case 'rdn':
+                if (args[2]) {
+                    if (isNormalInteger(args[2])) {
+                        msg.channel.send(Math.floor(Math.random() * args[2]))
+                    } else {
+                        msg.channel.send("Not an integer")
+                    }
+                }
+                break                
             case 'corona':
                 if (args[2]) {
+
                     let i = 0
-                    while( JSON.parse(countryCovid)[i].country != args[2].replace(/ /g, "_").replace(/\./g,"").replace(/\,/g,"").replace(/\-/g,"")) {
-                        i++ 
+            
+                    while( JSON.parse(countryCovid)[i].countryInfo.iso2 != args[2]) {
+                        console.log(i)
+                        if (i >= 220) break
+                        
+                        else i++ 
                     }
 
-                    let country = args[2].replace(/ /g, "_").replace(/\./g,"").replace(/\,/g,"").replace(/\-/g,"")
+                    if (i == 220) break; 
+
                     msg.channel.send(
-                        ":flag_" + countryCode[country].toLowerCase() + ": Country: " + JSON.parse(countryCovid)[i].country +
+                        ":flag_" + args[2].toLowerCase() + ": Country: " + countryCode[args[2]] +
                         "\n:mask: Cases: " + JSON.parse(countryCovid)[i].cases +
                         "\n:sneezing_face: Today cases: " + JSON.parse(countryCovid)[i].todayCases +
                         "\n:skull_crossbones: Deaths " + JSON.parse(countryCovid)[i].deaths +
@@ -124,4 +190,4 @@ bot.on('message', msg=> {
     
 })
 
-bot.login(token)
+bot.login(token.token)
